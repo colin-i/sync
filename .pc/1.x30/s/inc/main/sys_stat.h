45b94c5da5fc9441137340a58f6345b82706b552
sync <costin.b.84@gmail.com>
2023-05-28 06:43:10 +0300

    sync

---
+++ .pc/1.x30/s/inc/main/sys_stat.h
@@ -0,0 +1,10 @@
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int mkdir(const char*, mode_t);
+
+#ifdef __cplusplus
+}
+#endif
---
+++ .pc/1.x30/s/inc/main/time.h
@@ -0,0 +1,12 @@
+
+typedef long int time_t;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+time_t time(time_t);
+
+#ifdef __cplusplus
+}
+#endif
