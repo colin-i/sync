5c2c5892f56145cb36f2f9c15a1bc9d0a48251ee
sync <costin.b.84@gmail.com>
2023-06-26 06:38:31 +0300

    sync

---
+++ .pc/1.5+93/src/files/functions/pref/backup.s
@@ -0,0 +1,28 @@
+
+function pref_store()
+	sv src%nr_of_prefs_pointers_p
+	sd dest%prefs_backup_p
+
+	sd max=nr_of_prefs_jumper
+	add max src
+	while src!=max
+		sd p;set p src#
+		set dest# p#
+		incst src
+		add dest (dwsz)
+	endwhile
+endfunction
+
+function pref_restore()
+	sd src%prefs_backup_p
+	sv dest%nr_of_prefs_pointers_p
+
+	sd max=nr_of_prefs_jumper
+	add max dest
+	while dest!=max
+		sd p;set p dest#
+		set p# src#
+		incst dest
+		add src (dwsz)
+	endwhile
+endfunction
