e40407ccfbb3b7dceffdfdaf8568c7c85cb94c75
sync <costin.b.84@gmail.com>
2023-07-21 10:10:32 +0300

    sync

---
+++ .pc/1.x34/s/inc/main/unistd.h
@@ -0,0 +1,21 @@
+
+//sys/types.h
+typedef long off_t;
+
+//#include <bits/seek_constants.h>
+#define SEEK_SET 0
+#define SEEK_END 2
+
+#include "inc/unistd.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+off_t lseek(int,off_t,int);
+ssize_t read(int,void*,size_t);
+int unlink(const char*);
+
+#ifdef __cplusplus
+}
+#endif
