6c9f7a813cc5e160d2a62abe79ce8cd2fb6aafdd
sync <costin.b.84@gmail.com>
2023-12-22 15:26:11 +0200

    "sync"

---
+++ .pc/1.5+133/tests/xf/function.s
@@ -0,0 +1,13 @@
+
+format elfobj
+
+function test(char a,data b,value c,sv d,str e,ss f,vstr g,sd h,vdata *i,valuex j)
+endfunction
+
+functionx test2()
+endfunction
+
+functionxx test3()
+endfunction
+
+entry test4()
