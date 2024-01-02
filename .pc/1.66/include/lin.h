da65f02efc41f06b489b4c1c08a695cd42334a61
sync <costin.b.84@gmail.com>
2024-01-02 10:32:19 +0200

    "sync"

---
+++ .pc/1.66/include/lin.h
@@ -0,0 +1,8 @@
+
+Const flag_O_BINARY=0
+Const flag_O_CREAT=0x0040
+
+const S_IRUSR=0x100;const S_IWUSR=0x80
+const S_IRGRP=0x20;const S_IWGRP=0x10
+const S_IROTH=0x4;const S_IWOTH=0x2
+Const flag_pmode=S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH
---
+++ .pc/1.66/include/win.h
@@ -0,0 +1,4 @@
+
+const flag_O_CREAT=0x0100;const flag_O_BINARY=0x8000
+const flag_S_IREAD=0x100;const flag_S_IWRITE=0x80
+Const flag_pmode=flag_S_IREAD|flag_S_IWRITE
