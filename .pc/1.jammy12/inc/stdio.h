17f7e3d943a4a585abb2673d8a3904c860dae45a
sync <costin.b.84@gmail.com>
2024-02-23 08:58:36 +0200

    "sync"

---
+++ .pc/1.jammy12/inc/stdio.h
@@ -0,0 +1,19 @@
+
+#include"inc/stddef.h"
+
+#define EOF (-1)
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int getchar(void);
+int printf( const char * format, ... );
+int puts(const char*);
+int sscanf(const char *str, const char *format, ...);
+int snprintf(char* __buf, size_t __size, const char* __fmt, ...);
+int sprintf(char*,const char*,...);
+
+#ifdef __cplusplus
+}
+#endif
---
+++ .pc/1.jammy12/inc/time.h
@@ -0,0 +1,13 @@
+
+
+typedef long time_t;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+time_t time(time_t *t);
+
+#ifdef __cplusplus
+}
+#endif
