aea3527e6e9ecfebb03ed62c6a8af91a228f3d9a
sync <costin.b.84@gmail.com>
2024-05-25 10:42:06 +0300

    "sync"

---
+++ .pc/1.5+173/src/files/headers/ascii.h
@@ -0,0 +1,32 @@
+
+
+#some ascii Constants commonly used
+Const asciinul=0x0
+Const asciitab=0x9
+Const asciireturn=0xa
+Const asciicarriage=0xd
+Const asciispace=0x20
+Const asciidoublequote=0x22
+Const asciinumber=0x23
+Const asciiparenthesisstart=0x28
+Const asciiparenthesisend=0x29
+Const asciicomma=0x2C
+Const asciidot=0x2E
+Const asciizero=0x30
+Const asciione=0x31
+Const asciithree=0x33
+Const asciieight=0x38
+Const asciinine=0x39
+Const asciisemicolon=0x3B
+Const asciiaround=0x40
+Const asciiA=0x41
+Const asciiE=0x45
+Const asciiF=0x46
+Const asciiZ=0x5A
+Const asciiunderscore=0x5F
+Const asciif=0x66
+Const asciin=0x6E
+Const asciir=0x72
+Const asciit=0x74
+Const asciiz=0x7A
+Const asciiequiv=0x7E
