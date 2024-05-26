42181eba674cc3dddeb4bfad85b89ccc52883f07
sync <costin.b.84@gmail.com>
2024-05-26 07:25:29 +0300

    "sync"

---
+++ .pc/1.5+174/otoc/xfile.h
@@ -0,0 +1,2 @@
+einclude "../src/files/headers/xfile.h" "/usr/include/ocompiler/xfile.h"
+#einclude? will use all constants in the header. yes, but some are used without touching them, like in loop()
