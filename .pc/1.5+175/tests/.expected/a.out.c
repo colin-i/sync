cf2df98a4dd92cb87db246865d9f3376262e0019
sync <costin.b.84@gmail.com>
2024-05-28 11:55:21 +0300

    "sync"

---
+++ .pc/1.5+175/tests/.expected/a.out.c
@@ -0,0 +1,35 @@
+/*
+multi
+line
+*/
+//not blank for deb pack. char *="1\03" is not working (src->binary is creating fuzz at deb)
+int c11;
+int c223;
+int c3;
+int a3;
+int b;
+void intern(int a,int b){//commenting
+}
+void f();
+void main(){
+a=a;
+a+=a;
+a-=a;
+a*=a;
+a/=a;
+a/=a;
+a%=a;
+a%=a;
+a&=a;
+a|=a;
+a^=a;
+1a;intern(1,"qw",a);
+a=f();
+11{
+}
+12{
+}
+{
+}
+__builtin_trap();
+}
---
+++ .pc/1.5+175/tests/cond.oc
@@ -0,0 +1,26 @@
+
+format elfobj64
+entry main()
+if 1=1
+elseif 1=1
+endelseif
+while 1=1
+break
+continue
+endwhile
+
+if 1>=^1
+elseif 1<^1
+elseif 1!=1
+elseif 1=1
+elseif 1>^1
+elseif 1<=^1
+elseif 1>=1
+elseif 1<1
+elseif 1>1
+elseif 1<=1
+endelseif
+if 4!1
+end
+if 2!!1
+end
