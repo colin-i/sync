cdabe2550b6602d9b000b2e6edc337a0eb33e621
sync <costin.b.84@gmail.com>
2024-06-25 08:21:57 +0300

    "sync"

---
+++ .pc/1.5+180/tests/.expected/a.out.c
@@ -0,0 +1,35 @@
+/*
+multi
+line
+*/
+//not blank for deb pack. char *="1\03" is not working (src->binary is creating fuzz at deb)
+int c1=1;
+int c2[]={2,3};
+int c3="45";
+int a[3];
+int b;
+void intern(int a,int b,int c){//commenting
+}
+void f();
+void main(){
+b=b;
+b+=b;
+b-=b;
+b*=b;
+b/=b;
+b/=b;
+b%=b;
+b%=b;
+b&=b;
+b|=b;
+b^=b;
+1a;intern(1,"q\nw\re\tr",a);
+b=f();
+11{
+}
+12{
+}
+{
+}
+__builtin_trap();
+}
