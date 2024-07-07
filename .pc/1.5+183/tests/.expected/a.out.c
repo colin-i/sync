cde4437081f98aba46bc9998119e11f8c78b9069
sync <costin.b.84@gmail.com>
2024-07-07 09:32:39 +0300

    "sync"

---
+++ .pc/1.5+183/tests/.expected/a.out.c
@@ -0,0 +1,42 @@
+/*
+multi
+line
+*/
+#include "include.h"
+char c1=1;
+char c2[]={2,3};
+char c3[]="45";
+short c5=1;;int c6=1;;char* c7=1;;long c8=1;
+char* c9="";;short* ca=1;;int* cb=1;
+char cc[]={9,97,122};
+#define a1 1
+char a[3];
+long b;
+long intern(int a,int* b,long c){//commenting
+return c;
+}
+void f(){
+if(22){return;
+}
+}
+long puts();
+long main(){
+b=b;
+b+=b;
+b-=b;
+b*=b;
+b/=b;
+b/=b;
+b%=b;
+b%=b;
+b&=b;
+b|=b;
+b^=b;;intern(1,"q\nw\re\tr",a);
+b=puts();
+if(11){}
+while(12){}
+{
+}
+__builtin_trap();
+return 2;
+}
