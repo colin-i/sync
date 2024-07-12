21c401c0ef35ca08e907b5905509ea9092182c82
sync <costin.b.84@gmail.com>
2024-07-12 07:57:45 +0300

    "sync"

---
+++ .pc/1.5+185/otoc/ascii.h
@@ -0,0 +1,18 @@
+
+const asciinul=0x0
+const asciinewline=0xa
+const asciispace=0x20
+const asciidquote=0x22
+const asciiasterisk=0x2a
+const asciiparenthesisleft=0x28
+const asciiparenthesisright=0x29
+const asciicomma=0x2c
+const asciidot=0x2e
+const asciisemicolon=0x3b
+const asciiequal=0x3d
+const asciisquareleft=0x5B
+const asciisquareright=0x5D
+const asciibs=0x5C
+const asciic=0x63
+const asciicurlyleft=0x7b
+const asciicurlyright=0x7d
---
+++ .pc/1.5+185/tests/.expected/a.out.c
@@ -0,0 +1,46 @@
+/*
+multi
+line
+*/
+#include "include.h"
+char c1=1;
+char c2[]={2,3};
+char c3[]="45";
+short c5=1;;int c6=1;;char* c7=1;;long* c8=1;
+char* c9="";;short* ca=1;;int* cb=1;
+char cc[]={9,97,122};
+#define a1 1
+char a[3];
+long* b;
+long*intern(int a,int* b,long* c){//commenting
+a++;
+a--;
+c++;
+c--;
+return c;
+}
+void f(){
+if(22){return;
+}
+}
+long*puts();
+long*main(){
+b=b;
+b=(long)b+1;
+b=(long)b-1;
+b=(long)b*1;
+c1=(long)c1/1;
+c1=(long)c1/1;
+c1=(long)c1%1;
+c1=(long)c1%1;
+c1=(long)c1&1;
+c1=(long)c1|1;
+b=(long)b^1;;intern(1,"q\nw\re\tr",a);
+b=puts();
+if(11){}
+while(12){}
+{
+}
+__builtin_trap();
+return 2;
+}
