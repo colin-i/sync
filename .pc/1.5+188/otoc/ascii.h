b29cfabd0462244d918afaaeee0959dae3f50b23
sync <costin.b.84@gmail.com>
2024-07-18 15:34:10 +0300

    "sync"

---
+++ .pc/1.5+188/otoc/ascii.h
@@ -0,0 +1,26 @@
+
+const asciinul=0x0
+const asciinewline=0xa
+const asciispace=0x20
+const asciidquote=0x22
+const asciipercent=0x25
+const asciiasterisk=0x2a
+const asciiand=0x26
+const asciiparenthesisleft=0x28
+const asciiparenthesisright=0x29
+const asciiplus=0x2b
+const asciicomma=0x2c
+const asciihyphen=0x2d
+const asciidot=0x2e
+const asciisemicolon=0x3b
+const asciileft=0x3c
+const asciiright=0x3e
+const asciisquareleft=0x5b
+const asciibs=0x5c
+const asciisquareright=0x5d
+const asciicirc=0x5e
+const asciic=0x63
+const asciicurlyleft=0x7b
+const asciior=0x7c
+const asciicurlyright=0x7d
+const asciinot=0x7e
---
+++ .pc/1.5+188/tests/.expected/a.out.c
@@ -0,0 +1,71 @@
+/*
+multi
+line
+*/
+#include "include.h"
+char c1=1;
+char c2[]={2,3};
+char c3[]="45";
+short c5=1;int c6=1;char* c7=1;long* c8=1;
+char* c9="";short* ca=1;int* cb=1;
+char cc[]={9,97,122};
+#define a1 1
+char a[3];
+long* b;
+long*intern(int a,int* b,long* c){//commenting
+a++;
+a--;
+c++;
+c--;
+c=-(long)c;
+c=~(long)c;
+;
+;
+c=(long)c<<1;
+c=(unsigned long)c>>1;
+c=(long)c>>1;
+return c;
+};
+void f(){
+if(2==2){
+return;
+};
+};
+struct st{
+int a[1];
+};
+long*puts();
+long*main(){
+b=b;
+b=(long)b+1;
+b=(long)b-1;
+b=(long)b*1;
+c1=(long)c1/1;
+c1=(long)c1/1;
+c1=(long)c1%1;
+c1=(long)c1%1;
+c1=(long)c1&1;
+c1=(long)c1|1;
+b=(long)b^1;intern(1,"q\nw\re\tr",a);
+b=puts();
+if(1==1){
+//elseif 1!!1
+//elseif 1!1
+};
+if(1!=1){};
+if(1<1){};
+if(1>1){};
+if(1<=1){};
+if(1>=1){};
+if((unsigned long)1<(unsigned long)1){};
+if((unsigned long)1>(unsigned long)1){};
+if((unsigned long)1<=(unsigned long)1){};
+if((unsigned long)1>=(unsigned long)1){};
+while(1==2){
+};
+{
+};
+char z=0xff+077+12-3+(4*3/2/1&1|1^1%5%5<<3>>3>>3==2!=2<3>3<=4>=4&&7||7<6>6<=6>=6);//char er=3!!2 char er=3!2 char er=3$2
+char z2=a1;char z3=-a1;char z4=~a1;char z5=~-a1;__builtin_trap();
+return 2;
+};
