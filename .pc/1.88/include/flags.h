ce3f9c36e5b11f8fae382e0f256b472583e653a1
sync <costin.b.84@gmail.com>
2024-08-12 07:12:25 +0300

    "sync"

---
+++ .pc/1.88/include/flags.h
@@ -0,0 +1,13 @@
+
+const debug_none=0
+const debug_log=0x1
+const debug_alt=0x2
+const debug_titles=0x4
+const debug_x=0x8
+const debug_x_pad=0x10
+
+const flag_forin1=0x20
+const flag_framesAsShows=0x40
+const flag_x=0x80
+
+const flags_x=debug_x|debug_x_pad|flag_x
