07d02904cc0b69b0c6b4ee33b0847ac681c7ad7a
sync <costin.b.84@gmail.com>
2025-01-08 09:20:29 +0200

    "sync"

---
+++ .pc/1.5+213/tests/.expected/declareN.c
@@ -0,0 +1,24 @@
+void b(int d){
+char a[2];
+char c[]={1,2,3};
+//char e=""^
+char e[]="1234";
+int aa;
+aa;
+}
+//const a=\b.a\
+a=1;
+char z=;
+char z=;
+char z=;
+char z=1;
+char z=;
+char z=;
+char z=;
+char z=;
+char z=a;
+char z=c;
+char z=d;
+char z=e;
+char z=a;
+}
