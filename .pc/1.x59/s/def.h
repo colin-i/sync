a2d8d59c3e6d36c980ba69272dd548df539b1539
sync <costin.b.84@gmail.com>
2025-03-15 11:27:58 +0200

    "sync"

---
+++ .pc/1.x59/s/def.h
@@ -0,0 +1,20 @@
+
+//here because split.h is not inlcuding curses.h for WINDOW and base.h has that
+
+//main
+//bar,tw,tit,split
+extern row*rows;
+extern size_t rows_tot;
+//bar,split
+extern char ln_term[3];
+extern size_t ln_term_sz;
+//split
+extern size_t clue;
+
+//bar
+//split
+int open_or_new(char*);
+
+//sep
+//main,split
+extern char path_separator;
