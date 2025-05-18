9fd9f95df29c2b407309a22a4714bdba21c7daad
sync <costin.b.84@gmail.com>
2025-05-18 10:26:02 +0300

    "sync"

---
+++ .pc/1.x72/s/def.h
@@ -0,0 +1,26 @@
+
+//here because split.h is not inlcuding curses.h for WINDOW and base.h has that
+
+//main
+//bar,tw,tit,split
+extern row*rows;
+extern size_t rows_tot;
+//bar,split
+extern char ln_term[3];
+extern size_t ln_term_sz;
+//split
+extern size_t clue;
+
+//bar
+//split
+bool is_extension_ok(char*,char*);
+int open_or_new(char*);
+swrite_char wwrite(int,char*,row_dword,swrite_char(*)(int,void*,row_dword));
+
+//sep
+//main,split
+extern char path_separator;
+
+//split
+//bar
+#define swwrite(a,b,c) wwrite(a,b,c,swrite)
---
+++ .pc/1.x72/s/top.h
@@ -0,0 +1,42 @@
+
+#ifndef __cplusplus
+
+#ifdef HAVE_STDBOOL_H
+#include<stdbool.h>
+#else
+//
+#ifdef _Bool
+typedef typeof(_Bool) bool;
+#else
+typedef char bool;
+#endif
+//
+enum{false=0!=0,true=1==1};
+#endif
+
+#include "null.h"
+#endif
+
+
+//these are going also in split
+
+#define row_dword unsigned int
+
+typedef struct{
+	char*data;
+	row_dword spc;//at row_alloc(type,undo,...),pasting,enter, must be +1 for curses addstr and at split where right part is the file name
+	row_dword sz;//at memtrm to work more to avoid 64 bit rows?
+}row;
+//typedef struct {}rowwrap;
+
+#define swrite_bad -1
+#define swrite_ok 0
+#define swrite_char signed char
+
+#define extlen_size 1
+
+#define split_no -1
+#define split_err 0
+#define split_yes_mixless 1
+#define split_yes_mix 2
+#define split_char signed char
